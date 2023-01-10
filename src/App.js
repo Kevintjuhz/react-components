220693c818dcef67b808708a281572211157163a
kevinquaedvlieg <kevin@kqcreations.com>
2023-01-10 01:36:16 +0100

    Added a button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleClick = () => {
+        console.log("Click");
+    }
+    return (
+        <div>
+            <div>
+                <Button rounded primary onClick={handleClick} className="mb-5"><GoBell /> Click me</Button>
+            </div>
+            <div>
+                <Button outline secondary>Button 2</Button>
+            </div>
+            <div>
+                <Button success><GoCloudDownload /> Button 3</Button>
+            </div>
+            <div>
+                <Button warning>Button 4</Button>
+            </div>
+            <div>
+                <Button danger><GoDatabase /> Button 5</Button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
