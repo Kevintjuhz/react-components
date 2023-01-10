b1c0b0f9984ff91809d570985d10c2caf9cbfe9e
kevinquaedvlieg <kevin@kqcreations.com>
2023-01-10 22:20:23 +0100

    Added accordion component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import Button from "./Button";
-import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Accordion from "./components/Accordion";
 
 function App() {
-    const handleClick = () => {
-        console.log("Click");
-    }
+    const items = [
+        {
+            label: 'Can I use React on a project?',
+            content: 'You can use React on any project you want.',
+            id: '1',
+        },
+        {
+            label: 'Can I use Vue on a project?',
+            content: 'You can use Vue on any project you want.',
+            id: '2',
+        },
+        {
+            label: 'Can I use Angular on a project?',
+            content: 'You can use Angular on any project you want.',
+            id: '3',
+        }
+    ]
+
     return (
-        <div>
-            <div>
-                <Button rounded primary onClick={handleClick} className="mb-5"><GoBell /> Click me</Button>
-            </div>
-            <div>
-                <Button outline secondary>Button 2</Button>
-            </div>
-            <div>
-                <Button success><GoCloudDownload /> Button 3</Button>
-            </div>
-            <div>
-                <Button warning>Button 4</Button>
-            </div>
-            <div>
-                <Button danger><GoDatabase /> Button 5</Button>
-            </div>
-        </div>
+        <Accordion items={items}/>
     )
 }
 
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,29 @@
+import Button from "../components/Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+
+function ButtonPage() {
+    const handleClick = () => {
+        console.log("Click");
+    }
+    return (
+        <div>
+            <div>
+                <Button rounded primary onClick={handleClick} className="mb-5"><GoBell /> Click me</Button>
+            </div>
+            <div>
+                <Button outline secondary>Button 2</Button>
+            </div>
+            <div>
+                <Button success><GoCloudDownload /> Button 3</Button>
+            </div>
+            <div>
+                <Button warning>Button 4</Button>
+            </div>
+            <div>
+                <Button danger><GoDatabase /> Button 5</Button>
+            </div>
+        </div>
+    )
+}
+
+export default ButtonPage;
